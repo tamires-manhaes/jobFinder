07486f8d89f5b50cb35b1db23cfdae52242c7cb6
Tamires <tamiresmanhaes4@gmail.com>
2020-05-25 19:15:40 -0300

    testando conexão com db

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const db = require('./database/connection');
 
+// db connection
+db
+  .authenticate()
+  .then( ()=> {
+    console.log("sucessful connection with db");
+  }).catch(err => {
+    console.error("error try to connect to db");
+  });
+
+
+// routes
 app.get('/', (req, resp) => {
   resp.send("all good");
 });
 
+// porta 
 const port = 3000;
 
 app.listen(port, ()=> {
