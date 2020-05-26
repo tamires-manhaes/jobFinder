7e56c7ab45ac6d943bf9ba583569207c975f4259
Tamires <tamiresmanhaes4@gmail.com>
2020-05-25 23:09:54 -0300

    modulo exportado pra uso

--- models/Job.js
+++ models/Job.js
@@ -5,13 +5,13 @@ const Job = db.define('job', {
   title: {
     type: Sequelize.STRING,
   },
-  description: {
-    type: Sequelize.STRING,
-  },
   salary: {
     type: Sequelize.STRING,
   },
   company: {
+    type: Sequelize.STRING,
+  },
+  description: {
     type: Sequelize.STRING,
   },
   email: {
