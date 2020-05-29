6f64743cc2a555659a33f072e2ca6238d74af79b
Tamires <tamiresmanhaes4@gmail.com>
2020-05-29 18:12:59 -0300

    adiconando handlebars

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
 const db = require('./database/connection');
-const bodyparser = require('body-parser');
+const path = require('path');
+
+const Job = require('./models/Job');
 
 app.use(express.json());
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars({ defaultLayout: 'main' })); 
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // db connection
 db
@@ -16,7 +27,13 @@ db
 
 // routes
 app.get('/', (req, resp) => {
-  resp.send("all good");
+  Job.findAll({ order: [
+    ['createdAt', 'DESC']
+  ] })
+  .then(jobs => {
+    resp.render('index', { jobs });
+  });
+
 });
 
 app.use('/jobs', require('./routes/jobs'));
