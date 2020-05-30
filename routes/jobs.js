27ccdd8bbc0893516769493501cb8938443966de
Tamires <tamiresmanhaes4@gmail.com>
2020-05-30 15:02:04 -0300

    criando rota que manda form por db

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,29 +1,44 @@
 // todas as rotas referentes a jobs
-
 const express = require('express');
 const router = express.Router();
+const Job = require('../models/Job');
 
 router.get('/', (req, resp) => {
   resp.send('all good here');
 });
 
+// detalhe da vaga
+router.get('/view/:id', (req, resp) => {
+  Job.findOne({ 
+    where: { id: req.params.id }
+   }).then(job => {
+      resp.render('view', {
+        job
+      });
+   }).catch( err => console.log(err) );
+});
+
+// form de envio
 router.get('/add', (req, resp) => {
   resp.render('add');
-})
+});
+
 // adicionar um novo job
 router.post('/add', (req, resp) => {
-  const { title, salary, company, description, email, new_job } = req.body;
+  const { title, description, salary, company, email, new_job } = req.body;
 
-  job.create({
-    title,
-    salary,
-    company,
+  console.log(` title: ${title}`)
+
+  //inserir dados no sistema
+  Job.create({
+    title ,
     description,
-    email,
-    new_job
-  }).then( () => resp.json({ ok: true }) )
-    .catch(err => console.error(err));
-    
+    salary ,
+    company ,
+    email ,
+    new_job 
+  }).then( () => resp.redirect('/') ).catch(err => console.log(err) );
+
 });
 
 
