d6ba46218a4f58d0effcc8121875c6d4f3c33d80
Tamires <tamiresmanhaes4@gmail.com>
2020-05-30 15:02:21 -0300

    corrigindo bug no db

--- models/Job.js
+++ models/Job.js
@@ -1,25 +1,25 @@
 const Sequelize = require('sequelize');
 const db = require('../database/connection');
 
-const Job = db.define('job', {
-  title: {
-    type: Sequelize.STRING,
-  },
-  salary: {
-    type: Sequelize.STRING,
-  },
-  company: {
-    type: Sequelize.STRING,
-  },
-  description: {
-    type: Sequelize.STRING,
-  },
-  email: {
-    type: Sequelize.STRING,
-  },
-  new_job: {
-    type: Sequelize.INTEGER,
-  }
-});
+const Job = db.define('Job',{
+    title:{
+        type: Sequelize.STRING,
+    },
+    description:{
+        type: Sequelize.STRING,
+    },
+    salary: {
+        type: Sequelize.STRING,
+    },
+    company: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.STRING,
+    },
+    new_job: {
+        type: Sequelize.STRING,
+    }
+})
 
 module.exports = Job;
