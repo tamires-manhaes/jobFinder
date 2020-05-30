3bb3f5292affe0597fa604abedf2ff9642ba72a6
Tamires <tamiresmanhaes4@gmail.com>
2020-05-30 15:02:54 -0300

    utilizando o bodyparser

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
 const db = require('./database/connection');
+const bodyParser = require('body-parser');
 const path = require('path');
 const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-app.use(express.json());
+app.use( bodyParser.urlencoded({ extended: false }) );
 
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })); 
 app.set('view engine', 'handlebars');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jobs', require('./routes/jobs'));
 
 // db connection
 db
   .authenticate()
-  .then( ()=> {
+  .then( () => {
     console.log("sucessful connection with db");
   }).catch(err => {
     console.error("error try to connect to db");
@@ -26,20 +29,33 @@ db
 
 // routes
 app.get('/', (req, resp) => {
-  Job.findAll({ order: [
-    ['createdAt', 'DESC']
-  ] })
-  .then(jobs => {
-    resp.render('index', { jobs });
-  });
+
+  let search = req.query.job;
+  let query = '%'+search+'%';
+
+  if(!search) {
+    Job.findAll({ order: [
+      ['createdAt', 'DESC']
+    ] })
+    .then(jobs => {
+      resp.render('index', { jobs });
+    })
+    .catch(err => console.log(err));
+
+  } else {
+    Job.findAll({ 
+      where: { title: {[Op.like]: query } },
+      order: [
+        ['createdAt', 'DESC']
+    ] })
+    .then(jobs => {
+      resp.render('index', { jobs });
+    });
+  }
 
 });
-
-app.use('/jobs', require('./routes/jobs'));
 
 // porta 
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, ()=> {
-  console.log(`listen on port ${port}`);
-});
+app.listen( PORT, ()=> { console.log(`listen on port ${PORT}`); } );
